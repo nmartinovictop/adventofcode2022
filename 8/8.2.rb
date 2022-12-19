638adf83c0e3388a8221ecf6ece866b659a50a37
Nick Martinovic <nmartinovictop@gmail.com>
2022-12-19 20:15:08 +0900

    complete 8.2

--- 8/8.2.rb
+++ 8/8.2.rb
@@ -1,53 +1,150 @@
-file_data = File.read("prod.txt")
+file_data = File.read("prod.txt").split("\n").map { |x| x.split("")}
 
-boxes = [
-['B','V','S','N','T','C','H','Q'],
-['W','D','B','G'],
-['F','W','R','T','S','Q','B'],
-['L','G','W','S','Z','J','D','N'],
-['M','P','D','V','F'],
-['F','W','J'],
-['L','N','Q','B','J','V'],
-['G','T','R','C','J','Q','S','N'],
-['J','S','Q','C','W','D','M']
-]
-
-test_boxes = [
-  ['Z','N'],
-  ['M','C','D'],
-  ['P']
-]
-
-#['number to move','from_box','to_box']
-file_data = file_data.split("\n").map! { |x| x[5...x.length]}
-
-directions = [] 
-def clean_data(string)
-  arr = []
-
-  temp = string.split(" from ")
-  arr << temp[0]
-  temp = temp[1].split(" to ")
-  arr << temp
-  arr.flatten.map(&:to_i)
-end
-
-
-file_data.each do |string|
-  directions << clean_data(string)
-end
-
-directions.each do |direction|
-    boxes[direction[2]-1] += boxes[direction[1]-1].slice!((boxes[direction[1]-1].length-direction[0])...boxes[direction[1]-1].length)
-
+file_data.each do |ele|
+  ele.map!(&:to_i)
 end
 
 
 
+# [
+#   [3, 0, 3, 7, 3], 
+#   [2, 5, 5, 1, 2], 
+#   [6, 5, 3, 3, 2], 
+#   [3, 3, 5, 4, 9], 
+#   [3, 5, 3, 9, 0]
+# ]
 
 
-final = ''
-boxes.each do |box|
-  final += box.pop
+def vis_top(x,y,forest) #(2,2) ==> 1
+
+  counter = 0
+  if x == 0
+    return counter
+  end
+
+  max_tree = forest[x][y]
+  searcher = true
+
+  while searcher && x > 0
+    x -= 1
+    counter += 1
+    if forest[x][y] >= max_tree
+      searcher = false
+    end
+
+
+
+  end
+
+
+
+  return counter
 end
-p final
+
+
+def vis_bottom(x,y,forest)
+
+  counter = 0
+  if x == forest.length-1
+    return counter
+  end
+
+  max_tree = forest[x][y]
+  searcher = true
+
+  while searcher && x < forest.length-1
+    x += 1
+    counter += 1
+    if forest[x][y] >= max_tree
+      searcher = false
+    end
+
+
+
+  end
+
+
+
+  return counter
+end
+
+
+def vis_right(x,y,forest)
+
+  counter = 0
+  if y == forest[x].length-1
+    return counter
+  end
+
+  max_tree = forest[x][y]
+  searcher = true
+
+  while searcher && y < forest[x].length-1
+    y += 1
+    counter += 1
+    if forest[x][y] >= max_tree
+      searcher = false
+    end
+
+
+
+  end
+
+
+
+  return counter
+end
+
+def vis_left(x,y,forest) #(2,2) ==> 1
+
+  counter = 0
+  if y == 0
+    return counter
+  end
+
+  max_tree = forest[x][y]
+  searcher = true
+
+  while searcher && y > 0
+     y -= 1
+    counter += 1
+    if forest[x][y] >= max_tree
+      searcher = false
+    end
+
+
+
+  end
+
+
+
+  return counter
+end
+
+def array_of_views(x,y,forest)
+  scenic_score = 1
+  return_array = []
+  return_array << vis_top(x,y,forest)
+  return_array << vis_left(x,y,forest)
+  return_array << vis_right(x,y,forest)
+  return_array << vis_bottom(x,y,forest)
+
+  return_array.each do |ele|
+    scenic_score *= ele
+  end
+
+  scenic_score
+end
+
+counter = 0
+
+file_data.each_with_index do |ele_x,x_idx|
+  ele_x.each_with_index do |ele_y, y_idx|
+    tester = array_of_views(x_idx,y_idx,file_data)
+    if tester >= counter
+      counter = tester
+    end
+  end
+end
+
+p counter
